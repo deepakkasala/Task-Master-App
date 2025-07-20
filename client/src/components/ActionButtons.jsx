0cc63beccb6dc073632d815e8d771074551964e1
Deepak Kasala <deepakkasala14@gmail.com>
2025-07-20 11:10:24 +0530

    Commit from MAC

--- client/src/components/ActionButtons.jsx
+++ client/src/components/ActionButtons.jsx
@@ -19,13 +19,13 @@ const ActionButtons = ({
         <input
           type="text"
           placeholder="Search tasks..."
-          className="px-4 py-2 border border-red-900 dark:border-gray-700 rounded-lg w-full max-w-sm"
+          className="px-4 py-2 border border-gray-400 dark:border-gray-700 rounded-lg w-full max-w-sm"
           value={searchTerm}
           onChange={handleSearchChange}
         />
       </div>
 
-      <div className="flex justify-end gap-3 items-center mb-6">
+      <div className="flex justify-end gap-3 items-center mb-6 sm:mt-6">
         <button
           onClick={handleOpenModal}
           className="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg shadow"
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 const PORT = 3030;
-export const API_URL = `http://192.168.29.149:${PORT}`;
+export const API_URL = `http://localhost:${PORT}`;
